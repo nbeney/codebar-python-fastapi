fe81d99eedc9ec4f969daf5105d2d466beae54ce
nbeney <nbeney@gmail.com>
2023-03-13 20:18:26 +0000

    Add conjugate endpoint.

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/conjugate/{verb}")
+async def conjugate(verb: str):
+    base = verb[:-2]
+    return {
+        "verb": verb, 
+        "forms": [
+            ("je", base + "e"),
+            ("tu", base + "es"),
+            ("il/elle", base + "e"),
+            ("nous", base + "ons"),
+            ("vous", base + "ez"),
+            ("ils/elles", base + "ent"),
+        ]}
